577cc11877d245aacd62d27390bb4d5d5513d919
gusche85 <gishjode@gmail.com>
2024-02-18 08:32:14 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function App() {
+  return (
+    <main>
+      <div className="m-5 mb-5 border border-dark border-pill">
+      <p>hello</p>
+      </div>
+    </main>
+  )
+}
