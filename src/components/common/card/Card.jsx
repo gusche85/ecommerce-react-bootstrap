f5cb95555cf315ae54e8be18ce3013a65376ddc6
gusche85 <149746619+gusche85@users.noreply.github.com>
2024-02-21 08:00:17 +0000

    Add badge

--- src/components/common/card/Card.jsx
+++ src/components/common/card/Card.jsx
@@ -1,4 +1,4 @@
-import {Card, CardGroup, Button} from 'react-bootstrap';
+import {Card, CardGroup, Badge, Button} from 'react-bootstrap';
 
 function Product() {
   return (
@@ -36,14 +36,19 @@ function Product() {
       </Card>
 
       <Card className='align-items-center border text-center m-3' style={{ width: '18rem' }}>
+        <Badge bg="danger" style={{ position: 'absolute', top: '0', right: '0'}}>
+          Sale
+        </Badge>
         <Card.Img variant="top" className="object-fit-contain" src="paradoxe.png" height="210px" />
+        
         <Card.Body>
           <Card.Title>Prada</Card.Title>
           <Card.Text>
-          Paradoxe Eau De Parfum 30ml
+          Paradoxe Eau De Parfum 90ml
           </Card.Text>
           <Card.Text>
-            £69.00
+            <span className="text-decoration-line-through">£135.00</span>
+            <span className="text-danger"> £108.00</span>
           </Card.Text>
           <Card.Footer className="bg-transparent border-0">
           <Button variant="primary">Add to cart</Button>
@@ -75,6 +80,9 @@ function Product1 () {
   return (
     <CardGroup>
       <Card className='align-items-center border text-center m-3' style={{ width: '18rem' }}>
+        <Badge bg="danger" style={{ position: 'absolute', top: '0', right: '0'}}>
+          Sale
+        </Badge>
         <Card.Img variant="top" className="object-fit-contain" src="blackopium.png" height="210px" />
         <Card.Body >
           <Card.Title>Yves Saint Laurent</Card.Title>
@@ -82,7 +90,7 @@ function Product1 () {
             Black Opium Over Red Eau De Parfum 90ml
           </Card.Text>
           <Card.Text>
-          £131.00
+            <span className="text-decoration-line-through">£131.00</span><span className="text-danger"> £104.80</span>
           </Card.Text>
           <Card.Footer className="bg-transparent border-0">
           <Button variant="primary">Add to cart</Button>
@@ -92,12 +100,16 @@ function Product1 () {
 
       <Card className='align-items-center border text-center m-3' style={{ width: '18rem' }}>
         <Card.Img variant="top" className="object-fit-contain" src="dolce.png" height="210px" />
+        <Badge bg="danger" style={{ position: 'absolute', top: '0', right: '0'}}>
+          Sale
+        </Badge>
         <Card.Body>
           <Card.Title>Dolce & Gabbana</Card.Title>
           <Card.Text>
             The Only One Eau De Parfum 50ml
           </Card.Text>
-          <Card.Text>£55.00</Card.Text>
+          <Card.Text><span className="text-decoration-line-through">£78.00</span> <span className="text-danger">£55.00</span>
+</Card.Text>
           <Card.Footer className="bg-transparent border-0">
           <Button variant="primary">Add to cart</Button>
           </Card.Footer>
